2eeb4f9737dce291a4e440e03d89777bcfcfdd53
CJenkins <c.e.jenkins001@gmail.com>
2024-02-07 16:06:38 +0000

    Responsive navigation

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 
 
+//Page Clock
+
 setInterval(() => {
   document.getElementById("ldnTime").innerHTML = new Date().toLocaleString("en-Us", {timeZone:'Europe/London', timeStyle:'medium',hourCycle:'h24'});
 }, 1000);
+
+//Navigation Toggle.
+
+const navigation = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navToggle')
+
+navToggle.addEventListener("click", () =>
+{
+const visibility = navigation.getAttribute('data-visible');
+
+if(visibility === "false"){
+  navigation.setAttribute('data-visible', true);
+} else if (visibility==="true") {
+  navigation.setAttribute('data-visible', false);
+}
+
+});
