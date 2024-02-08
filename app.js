90aa183124ef7394fdf42c6f0cbf5291ccd0fe7b
CJenkins <c.e.jenkins001@gmail.com>
2024-02-08 16:38:51 +0000

    Background Features
    Home page -
    
    Grass,
    Sign,
    Sword,
    swordHover,
    moveable Sword.

--- app.js
+++ app.js
@@ -22,3 +22,85 @@ if(visibility === "false"){
 }
 
 });
+
+
+
+//Move sword
+
+
+let moveable = document.getElementById("move");
+let initialX =0;
+let initialY =0;
+
+let moveElement = false;
+
+
+let events = {
+  mouse : {
+    down: "mousedown",
+    move: "mousemove",
+    up: "mouseup",
+  },
+  touch: {
+    down: "touchstart",
+    move: "touchmove",
+    up: "touchend",
+  },
+};
+
+let deviceType = "";
+
+const isTouchDevice = () => {
+  try{
+    document.createEvent("TouchEvent");
+    deviceType="touch";
+    return true;
+  }
+  catch(e)
+  {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+isTouchDevice();
+
+
+moveable.addEventListener(events[deviceType].down, (e) =>
+{
+  e.preventDefault();
+
+  initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+  initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+  moveElement = true;
+
+});
+
+moveable.addEventListener(events[deviceType].move,(e) => {
+
+  if(moveElement) {
+    e.preventDefault();
+    let newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+    let newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+    moveable.style.top = moveable.offsetTop - (initialY - newY) + "px";
+    moveable.style.left = moveable.offsetLeft - (initialX - newX) + "px";
+
+    initialX = newX;
+    initialY = newY;
+  }
+});
+
+
+moveable.addEventListener(
+  events[deviceType].up,
+  (stopMovement = (e) => {
+    moveElement = false;
+  })
+)
+
+moveable.addEventListener("mouseleave", stopMovement);
+moveable.addEventListener(events[deviceType].up, (e)=>{
+moveElement = false;
+})
